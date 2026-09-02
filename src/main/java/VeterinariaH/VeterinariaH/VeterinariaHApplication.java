7cf2d8beedbb2a6da8a2d632dbe2dd728f3a1dc5
cristian200722 <cristianhernandez200722@gmail.com>
2026-09-02 08:56:52 -0500

    first commit

---
+++ src/main/java/VeterinariaH/VeterinariaH/VeterinariaHApplication.java
@@ -0,0 +1,13 @@
+package VeterinariaH.VeterinariaH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaHApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaHApplication.class, args);
+	}
+
+}
---
+++ src/test/java/VeterinariaH/VeterinariaH/VeterinariaHApplicationTests.java
@@ -0,0 +1,13 @@
+package VeterinariaH.VeterinariaH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaHApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
