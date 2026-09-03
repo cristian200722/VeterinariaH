3d2f63a296169b1d4911e4265238aa2d81b619b5
cristian200722 <cristianhernandez200722@gmail.com>
2026-09-02 22:19:17 -0500

    fix: corregir version de Spring Boot en pom.xml a 3.4.0 y dependencias de test

--- src/test/java/com/veterinariah/cliente/controller/ClienteControllerTest.java
+++ src/test/java/com/veterinariah/cliente/controller/ClienteControllerTest.java
@@ -11,8 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.boot.test.mock.mockito.MockBean;import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
@@ -28,7 +28,7 @@ class ClienteControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     private ClienteService clienteService;
 
     @Autowired
